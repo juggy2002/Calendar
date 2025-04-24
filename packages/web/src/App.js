4f7934b9f6e8d4d73d74aba6863e175a6f7ead1b
juggy2002 <singhjugaad@icloud.com>
2025-04-24 23:25:56 +0100

    r

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -13,7 +13,6 @@ import LoginPage from './LoginPage';
 import HomePage from './HomePage';             // admin dashboard
 import CreateUserPage from './CreateUserPage';
 import PortalPage from './PortalPage';         // regular user portal
-import { Analytics } from "@vercel/analytics/react" // vercel analytics
 
 function AppRoutes() {
   const [user, setUser] = useState(null);
