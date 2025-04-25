16790ccb914e8ca2ad4d6d1ff4b98c427e4c1693
juggy2002 <singhjugaad@icloud.com>
2025-04-25 15:35:03 +0100

    Update index.js

--- packages/api/index.js
+++ packages/api/index.js
@@ -265,6 +265,8 @@ app.post('/messages/:id/read', isAuthenticated, (req, res) => {
 
 const OpenAI = require('openai');
 
+console.log("OPENAI_API_KEY:", process.env.OPENAI_API_KEY);
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
